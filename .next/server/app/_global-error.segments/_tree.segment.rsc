b172f907aaad4b9a7577678f8c8a7bb4881ed164
Rahul Singhai <rahul29may2000@gmail.com>
2026-08-11 15:24:34 +0530

    Deploy bundle: resilience + learning loop

--- .next/server/app/_global-error.segments/_tree.segment.rsc
+++ .next/server/app/_global-error.segments/_tree.segment.rsc
@@ -1,1 +1,1 @@
-0:{"tree":{"name":"","param":null,"prefetchHints":0,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":0,"slots":null}}},"staleTime":300,"buildId":"5imow95ir2dzNfmgKlPN2"}
+0:{"tree":{"name":"","param":null,"prefetchHints":0,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":0,"slots":null}}},"staleTime":300,"buildId":"iuLbNSpI-298-rQPYt79z"}
--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -7,9 +7,9 @@ globalThis.__BUILD_MANIFEST = {
     "static/chunks/0cz1d0mv5g_q7.js"
   ],
   "lowPriorityFiles": [
-    "static/5imow95ir2dzNfmgKlPN2/_buildManifest.js",
-    "static/5imow95ir2dzNfmgKlPN2/_ssgManifest.js",
-    "static/5imow95ir2dzNfmgKlPN2/_clientMiddlewareManifest.js"
+    "static/iuLbNSpI-298-rQPYt79z/_buildManifest.js",
+    "static/iuLbNSpI-298-rQPYt79z/_ssgManifest.js",
+    "static/iuLbNSpI-298-rQPYt79z/_clientMiddlewareManifest.js"
   ],
   "rootMainFiles": [
     "static/chunks/3ll07n217cgxf.js",
