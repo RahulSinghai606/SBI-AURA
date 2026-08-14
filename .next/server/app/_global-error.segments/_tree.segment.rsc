3a09db0f9d5531e9f1ae9622a1c31103352b4700
Rahul Singhai <rahul29may2000@gmail.com>
2026-08-14 13:12:58 +0530

    Deploy bundle: pending-service fast path

--- .next/server/app/_global-error.segments/_tree.segment.rsc
+++ .next/server/app/_global-error.segments/_tree.segment.rsc
@@ -1,1 +1,1 @@
-0:{"tree":{"name":"","param":null,"prefetchHints":0,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":0,"slots":null}}},"staleTime":300,"buildId":"wwvq8K1eF63hAXDtNn8jd"}
+0:{"tree":{"name":"","param":null,"prefetchHints":0,"slots":{"children":{"name":"__PAGE__","param":null,"prefetchHints":0,"slots":null}}},"staleTime":300,"buildId":"q1NpTLjmxfWEgZlYys_Fk"}
--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -7,9 +7,9 @@ globalThis.__BUILD_MANIFEST = {
     "static/chunks/0cz1d0mv5g_q7.js"
   ],
   "lowPriorityFiles": [
-    "static/wwvq8K1eF63hAXDtNn8jd/_buildManifest.js",
-    "static/wwvq8K1eF63hAXDtNn8jd/_ssgManifest.js",
-    "static/wwvq8K1eF63hAXDtNn8jd/_clientMiddlewareManifest.js"
+    "static/q1NpTLjmxfWEgZlYys_Fk/_buildManifest.js",
+    "static/q1NpTLjmxfWEgZlYys_Fk/_ssgManifest.js",
+    "static/q1NpTLjmxfWEgZlYys_Fk/_clientMiddlewareManifest.js"
   ],
   "rootMainFiles": [
     "static/chunks/3ll07n217cgxf.js",
